9a0dca929512b1db4c63df52f95242cb224655ea
Chris Kjellqvist <ckjellqv@node2x10c.cs.rochester.edu>
2019-09-23 11:16:08 -0400

    up to date

---
+++ plib/plib.c
@@ -0,0 +1,42 @@
+#include "plib.h" 
+#include <stdio.h>
+#include <errno.h>
+/* Return the value of the PKRU register.  */
+static inline unsigned int
+pkey_read(void) {
+  unsigned int result;
+  __asm__ volatile (".byte 0x0f, 0x01, 0xee"
+      : "=a" (result) : "c" (0) : "rdx");
+  return result;
+}
+
+/* Overwrite the PKRU register with VALUE.  */
+static inline void
+pkey_write(unsigned int value) {
+  __asm__ volatile (".byte 0x0f, 0x01, 0xef"
+      : : "a" (value), "c" (0), "d" (0));
+}
+
+inline int pkey_set(int key, unsigned int rights) {
+  if (key < 0 || key > 15 || rights > 3) {
+    __set_errno (EINVAL);
+    return -1;
+  }
+  unsigned int mask = 3 << (2 * key);
+  unsigned int pkru = pkey_read ();
+  pkru = (pkru & ~mask) | (rights << (2 * key));
+  pkey_write (pkru);
+  return 0;
+}
+
+inline int pkey_mprotect(void *addr, size_t len, int prot, int pkey) {
+  return syscall(329, addr, len, prot, pkey);
+}
+
+inline int pkey_alloc(unsigned long flags, unsigned long access_rights) {
+  return syscall(330, flags, access_rights);
+}
+inline int pkey_free(int pkey) {
+  return syscall(331, pkey);
+}
+
