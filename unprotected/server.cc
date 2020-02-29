9c646bb52a684183de24f5c50e45ba50495000bc
Wentao Cai <wcai6@cs.rochester.edu>
2020-02-28 21:53:14 -0500

    pmdk thing

--- unprotected/server.cc
+++ unprotected/server.cc
@@ -4,13 +4,6 @@
 extern bool server_flag;
 int main(){
   server_flag = true;
-#ifdef PMDK
-  pop = pmemobj_create(HEAP_FILE, "test", REGION_SIZE, 0666);
-  if (pop == nullptr) {
-    perror("pmemobj_create");
-    return 1;
-  }
-#endif
   memcached_init();
   memcached_start_server();
 }
